267ebf1773c09a5628534cad37882eec53ba5483
Roshan Varghese Chacko <166170097+cetmulearnrvc@users.noreply.github.com>
2025-07-26 10:33:37 +0530

    Removed conflict  config.dart

--- lib/screens/LIC/pvr1/config.dart
+++ lib/screens/LIC/pvr1/config.dart
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-const url = "http://192.168.29.46:3000/api/v1/pvr1/generatepdf";
 
-<<<<<<< HEAD
-const url2 = "http://192.168.29.46:3000/api/v2/getNearby";
-
-const url3 = "http://192.168.29.46:3000/api/v1/pvr1/getByDate";
-
-const url4 = "http://192.168.29.46:3000/uploads/";
-
-const url5 = "http://192.168.29.46:3000/api/v2/saveNearby";
-=======
-const url = "http://192.168.29.47:3000/api/v1/pvr1/generatepdf";
-
-const url2 = "http://192.168.29.47:3000/api/v2/getNearby";
-
-const url3 = "http://192.168.29.47:3000/api/v1/pvr1/getByDate";
-
-const url4 = "http://192.168.29.47:3000/uploads/";
-
-const url5 = "http://192.168.29.47:3000/api/v2/saveNearby";
->>>>>>> ddf1111d69185a50a56bde3a891e828c23c4dc86
-=======
 const url = "http://localhost:3000/api/v1/pvr1/generatepdf";
 
 const url2 = "http://localhost:3000/api/v2/getNearby";
@@ -30,4 +8,3 @@ const url3 = "http://localhost:3000/api/v1/pvr1/getByDate";
 const url4 = "http://localhost:3000/uploads/";
 
 const url5 = "http://localhost:3000/api/v2/saveNearby";
->>>>>>> 56962f93bb65c1eed0ca5ef5896f286f7b40f297
