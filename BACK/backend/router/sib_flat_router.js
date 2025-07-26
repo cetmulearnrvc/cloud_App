7269357b0e4009454d464a6d6684efb8d30b229c
Roshan Varghese Chacko <166170097+cetmulearnrvc@users.noreply.github.com>
2025-07-26 10:24:33 +0530

    Removed conflict  sib_flat_router.js

--- BACK/backend/router/sib_flat_router.js
+++ BACK/backend/router/sib_flat_router.js
@@ -1,14 +1,10 @@
 import express from "express";
 const flat_router=express.Router();
 import { saveFlatData } from "../controller/sib_flat_controller.js";
-<<<<<<< HEAD
+
 import uploadMiddleware from "../multer/upload.js";
-import { getNearbyFlat, searchByDate } from "../controller/sib_flat_controller.js";
-import uploadMiddleware from "../multer/upload.js";
-=======
-import upload from "../multer/upload.js";
+// import upload from "../multer/upload.js";
 import { searchByDate } from "../controller/sib_flat_controller.js";
->>>>>>> 56962f93bb65c1eed0ca5ef5896f286f7b40f297
 // import { searchByDate, searchByFileNo } from "../controller/search.controller.js";
 
 flat_router.post("/flat/savepdf", uploadMiddleware ,saveFlatData);
