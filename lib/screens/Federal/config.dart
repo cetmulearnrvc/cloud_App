9d71fd183ed09f591bc811673024b9bf19096ed2
Roshan Varghese Chacko <166170097+cetmulearnrvc@users.noreply.github.com>
2025-07-26 10:30:07 +0530

    Removed conflict  config.dart

--- lib/screens/Federal/config.dart
+++ lib/screens/Federal/config.dart
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const url1 = "http://192.168.29.46:3000/api/v1/federal/save";
 
-const url2 = "http://192.168.29.46:3000/api/v1/federal/getnearby";
-
-const url3 = "http://192.168.29.46:3000/api/v1/federal/getByDate";
-
-const url4 = "http://192.168.29.46:3000/uploads/";
-=======
-const url1 = "http://192.168.29.47:3000/api/v1/federal/save";
-
-const url2 = "http://192.168.29.47:3000/api/v1/federal/getnearby";
-
-const url3 = "http://192.168.29.47:3000/api/v1/federal/getByDate";
-
-const url4 = "http://192.168.29.47:3000/uploads/";
->>>>>>> ddf1111d69185a50a56bde3a891e828c23c4dc86
-=======
 const url1 = "http://localhost:3000/api/v1/federal/save";
 
 const url2 = "http://localhost:3000/api/v2/getNearby";
@@ -26,4 +8,3 @@ const url3 = "http://localhost:3000/api/v1/federal/getByDate";
 const url4 = "http://localhost:3000/uploads/";
 
 const url5 = "http://localhost:3000/api/v2/saveNearby";
->>>>>>> 56962f93bb65c1eed0ca5ef5896f286f7b40f297
