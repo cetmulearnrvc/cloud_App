fd4f0f8c33dbe520380a68611dd33a5fa7f7b9b6
Roshan Varghese Chacko <166170097+cetmulearnrvc@users.noreply.github.com>
2025-07-26 10:25:37 +0530

    Removed conflict sib_land_router.js

--- BACK/backend/router/sib_land_router.js
+++ BACK/backend/router/sib_land_router.js
@@ -1,13 +1,8 @@
 import express from "express";
 const land_router=express.Router();
-<<<<<<< HEAD
 import uploadMiddleware from "../multer/upload.js";
-import { getNearbySIB, savelandData, searchByDate } from "../controller/sib_land_controller.js";
-import uploadMiddleware from "../multer/upload.js";
-=======
-import upload from "../multer/upload.js";
+// import upload from "../multer/upload.js";
 import {  savelandData, searchByDate } from "../controller/sib_land_controller.js";
->>>>>>> 56962f93bb65c1eed0ca5ef5896f286f7b40f297
 
 // import { searchByDate, searchByFileNo } from "../controller/search.controller.js";
 
@@ -16,7 +11,6 @@ land_router.post("/land/save", uploadMiddleware ,savelandData);
 land_router.post("/land/getByDate",searchByDate);
 
 //  land_router.post("/land/getnearby",getNearbySIB);
-
 
 // pvr1_router.post("/pvr1/getByFile",searchByFileNo);
 
